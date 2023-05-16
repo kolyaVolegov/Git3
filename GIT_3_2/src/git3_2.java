5bd595a8c8886551341553a0e484d28488e88cd9
Nikolay Volegov <kolyavolegov@gmail.com>
2023-05-16 17:14:12 +0300

    initil git reset

---
+++ GIT_3_2/src/git3_2.java
@@ -0,0 +1,7 @@
+public class git3_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Дополнительные изменения");
+        System.out.println("git reset, эта фраза должна исчезнуть");
+    }
+}
